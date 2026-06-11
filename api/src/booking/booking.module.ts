aabee43634de53c1488e27390cdb65d2ae501979
arfrhmshrtn <iw8628940@gmail.com>
2026-06-12 03:51:47 +0700

    refactor(booking): hapus komentar import MidtransModule di booking.module.ts

--- api/src/booking/booking.module.ts
+++ api/src/booking/booking.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
-import { MidtransModule } from '../midtrans/midtrans.module'; // 🔥 import ini
+import { MidtransModule } from '../midtrans/midtrans.module';
 
 @Module({
   imports: [MidtransModule], // 🔥 WAJIB
