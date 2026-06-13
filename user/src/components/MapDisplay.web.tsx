0e84db7fb7b1e3d9eb1870b01812c02a186ad9eb
GeevanAlv <recsa0210@gmail.com>
2026-06-13 23:00:03 +0700

    feat(web): mendefinisikan tipe data props konsisten dengan versi native

--- user/src/components/MapDisplay.web.tsx
+++ user/src/components/MapDisplay.web.tsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+
+interface MapDisplayProps {
+  latitude: string;
+  longitude: string;
+  name: string;
+  location: string;
+}
