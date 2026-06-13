0c811d367a0424dc112dec5601f16ead07cb4b3e
GeevanAlv <recsa0210@gmail.com>
2026-06-13 22:53:04 +0700

    feat(components): mengatur titik kordinat awal peta berdasarkan data dinamis

--- user/src/components/MapDisplay.tsx
+++ user/src/components/MapDisplay.tsx
@@ -12,9 +12,17 @@ interface MapDisplayProps {
 export default function MapDisplay({ latitude, longitude, name, location }: MapDisplayProps) {
   return (
     <View>
-        <MapView style={styles.map}>
+        <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
         
-        </MapView>
+      </MapView>
     </View>
   );
 }
