16e34e9b3fc38d1c5b5c9f9c8863449997f9ea1c
GeevanAlv <recsa0210@gmail.com>
2026-06-13 23:01:02 +0700

    style(web): menyiapkan struktur objek stylesheet untuk web fallback

--- user/src/components/MapDisplay.web.tsx
+++ user/src/components/MapDisplay.web.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface MapDisplayProps {
   latitude: string;
@@ -15,3 +16,8 @@ export default function MapDisplay({ name }: MapDisplayProps) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mapContainer: {},
+  textWarning: {}
+});
